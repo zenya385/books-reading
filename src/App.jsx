e5b1f0bb1d3b70fc1263e13e9f7481b7bb69199a
Roman Zeno <zeno385@gmail.com>
2022-04-06 16:50:11 +0300

    Timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./index.css";
+// import React from "react";
+
 import React, { Suspense, lazy } from "react";
 import { Redirect, Switch } from "react-router-dom";
 import AppBar from "./components/navigation/AppBar";
@@ -6,6 +8,9 @@ import PrivateRoute from "./components/Routs/PrivateRoute";
 import PublicRoute from "./components/Routs/PublicRoute";
 import { Route } from "react-router-dom";
 import BookInfoList from "./components/BookInfoList/BookInfoList";
+// import TimerLogicOfYear from "./components/Timer/TimerLogicOfYear/TimerLogicOfYear";
+// import TimerLogicOfGoals from "./components/Timer/TimerLogicOfGoals/TimerLogicOfGoals";
+import Timer from "./components/Timer/timer/Timer";
 
 const LoginPage = lazy(() => import("./pages/LoginPage.jsx"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage.jsx"));
@@ -16,6 +21,7 @@ export default function App() {
   return (
     <>
       <AppBar />
+      <Timer />
 
       <Suspense fallback={<h1>Wait a second, please =)</h1>}>
         <Switch>
